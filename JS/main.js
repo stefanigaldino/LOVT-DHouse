1d83209d13a8a270fcc4b5e799455e2e645a5949
Stefani Galdino <stefanijuris@hotmail.com>
2021-04-27 09:56:38 -0300

    mode noturno

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,25 @@
 console.log('javascript funcionando!');
 
-console.log(window);
+//let adulto = confirm('Você tem mais de 18 anos?'); //retorna um booleano
 
-alert('Seja bem vindo!')
+let nomeUsuario = prompt('Qual é seu nome?'); //retorna texto escrito pelo usuario
+
+console.log(nomeUsuario);
+
+let elementHeader = document.querySelector('.olaUsuario');
+// elementHeader.innerText = `Olá, ${nomeUsuario}`;
+console.log(elementHeader);
+
+elementHeader.innerHTML += `<strong>Olá, ${nomeUsuario}</strong>`;
+
+let logo = document.getElementById('ola');
+console.log(ola);
+
+ola.style.color = "Orange"
+
+document.getElementById("ola").onclick = function() {darkMode()};
+
+function darkMode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
